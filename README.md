1dc510c3a9e513ba64cfefc0d41b0f6b05a1c5e3
Jamous Bitrick <jbitrick@biznet.bnt.com>
2024-05-06 21:13:20 +0100

    test

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ Also returns vendor.
 You can pass the command at runtime, or when prompted.
 
 Windows an linux version are included in bin.
+
+
+Example
+-------
+```
+$ ./mac aa-00-00-f0-ed-86
+DIGITAL EQUIPMENT
+aa-00-00-f0-ed-86
+aa00.00f0.ed86
+aa:00:00:f0:ed:86
+AA:00:00:F0:ED:86
+```
--- main.go
+++ main.go
@@ -10,39 +10,78 @@ import (
 )
 
 func main() {
+	//Get input
 	input := parseInput()
 
-	//Convert to clean mac
-	macMap, clean := convertMac(input)
-	fmt.Println(macMap)
+	//Determin if a single input was passed, or a multiple imput. returns bool
+	single := singleInputCheck(input)
+	fmt.Println(single)
 
-	//Find the mac vendor
-	vendor := findVend(clean)
-
-	//Print results
-	fmt.Println(vendor)
-	fmt.Println(macMap["cisco"])
-	fmt.Println(macMap["lowercase"])
-	fmt.Println(macMap["uppercase"])
+/*
+	//If only one input, process as single input
+	if len(inputs) == 1 {
+		singleInput(inputs[0])
+	} else {
+	//Assume there are multipe inputs
+		multipleInput(inputs)
+	}
+	*/
 }
 
+//Parse inputs
 func parseInput() string {
 	var input string
-
-	//Prompt if impot was not passed at runtime
+	
+	//If onne value was wassed at runtime, assume this is the address
 	if len(os.Args) >= 2 {
 		input = os.Args[1]
-	} else {
-		fmt.Println("\nMac address to convert: ")
+	
+	} else { 
+	//If no values were passed at runtime, prompt the user for single address, or multi address lookup
+		fmt.Println("\nMac address or addresses to convert: ")	
 		reader := bufio.NewReader(os.Stdin)
 		dinput, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal("Could not get mac address from user.")
 		}
 		input = strings.TrimSpace(dinput)
+	
 	}
 
 	return input
+}
+
+//Determin if a single input was passed, or a multiple imput.
+func singleInputCheck(input string) bool {
+	//Check how many new line characters are in the string
+	lineCount := strings.Count(input, "\n")
+	fmt.Println(lineCount)
+
+	return false
+
+
+}
+
+func singleInput(input string) {
+	//Convert to clean mac
+	macMap, clean := convertMac(input)
+
+	//Find the mac vendor
+	vendor := findVend(clean)
+
+	//Print results
+	fmt.Println(vendor)
+	fmt.Println(macMap["dashed"])
+	fmt.Println(macMap["cisco"])
+	fmt.Println(macMap["lowercase"])
+	fmt.Println(macMap["uppercase"])
+}
+
+func multipleInput(inputs []string) {
+	
+	//Print results
+	fmt.Println(inputs)
+
 }
 
 func convertMac(input string) (map[string]string, string) {
@@ -60,6 +99,7 @@ func convertMac(input string) (map[string]string, string) {
 
 	//Convert to useable formats
 	macMap := map[string]string{
+		"dashed":    fmt.Sprintf("%s-%s-%s-%s-%s-%s", input[:2], input[2:4], input[4:6], input[6:8], input[8:10], input[10:]),
 		"cisco":     fmt.Sprintf("%s.%s.%s", input[:4], input[4:8], input[8:]),
 		"lowercase": fmt.Sprintf("%s:%s:%s:%s:%s:%s", input[:2], input[2:4], input[4:6], input[6:8], input[8:10], input[10:]),
 		"uppercase": strings.ToUpper(fmt.Sprintf("%s:%s:%s:%s:%s:%s", input[:2], input[2:4], input[4:6], input[6:8], input[8:10], input[10:])),
